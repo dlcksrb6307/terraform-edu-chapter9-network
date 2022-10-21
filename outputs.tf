dcec3085f9abf806ac83cd7805ddb535893b91bd
dlcksrb6307 <dlcksrb6307@lguplus.co.kr>
2022-10-21 10:46:59 +0900

    add output for subnet and security group

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,13 @@
 # output "catapp_ip" {
 #   value = "http://${aws_eip.hashicat.public_ip}"
 # }
+
+output "subnet_id_hashicat" {
+  value = aws_subnet.hashicat.id
+  description = "The hashicat's subnet id"
+}
+
+output "security_group_id_hashicat" {
+  value = aws_security_group.hashicat.id
+  description = "The hashicat's security group id"
+}
